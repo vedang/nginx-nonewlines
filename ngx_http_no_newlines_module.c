19baf6ff64afa73b69a5ad457728928d9edefb54
Vedang Manerikar <ved.manerikar@gmail.com>
2010-07-15 17:36:10 +0530

    Added the module definition block

--- ngx_http_no_newlines_module.c
+++ ngx_http_no_newlines_module.c
@@ -34,3 +34,18 @@ static ngx_http_module_t  ngx_http_no_newlines_module_ctx = {
     NULL                           /* merge location configuration */
 };
 
+/* The Module Definition - the master control */
+ngx_module_t ngx_http_no_newlines_module = {
+    NGX_MODULE_V1,
+    &ngx_http_no_newlines_module_ctx, /* module context */
+    ngx_http_no_newlines_commands,    /* module directives */
+    NGX_HTTP_MODULE,                  /* module type */
+    NULL,                             /* init master */
+    NULL,                             /* init module */
+    NULL,                             /* init process */
+    NULL,                             /* init thread */
+    NULL,                             /* exit thread */
+    NULL,                             /* exit process */
+    NULL,                             /* exit master */
+    NGX_MODULE_V1_PADDING
+};
