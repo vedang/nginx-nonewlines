13e92118024f972297feec5418d68750a440eb37
Vedang Manerikar <ved.manerikar@gmail.com>
2010-07-18 20:11:30 +0530

    Added worker functions to ignore preformatted text

--- ngx_http_no_newlines_module.c
+++ ngx_http_no_newlines_module.c
@@ -16,33 +16,40 @@ typedef struct {
 /* Enum defining states required */
 typedef enum {
     state_text = 0,
-    state_abort
+    state_abort,
+    state_tag_pre
 } ngx_http_no_newlines_state_e;
 
 
 /* Setup function for the no_newlines directive */
+/*
 static char *ngx_http_no_newlines (ngx_conf_t *cf,
                                    ngx_command_t *cmd,
                                    void *conf);
+*/
 /* Handler function */
+/*
 static ngx_int_t ngx_http_no_newlines_handler (ngx_http_request_t *r);
+*/
 /* Header filter */
 static ngx_int_t ngx_http_no_newlines_header_filter (ngx_http_request_t *r);
 /* Body filter */
 static ngx_int_t ngx_http_no_newlines_body_filter (ngx_http_request_t *r,
                                                    ngx_chain_t *in);
-/* Worker function */
-static void ngx_http_no_newlines_strip_buffer (ngx_buf_t *buffer,
-                                               ngx_http_no_newlines_ctx_t *ctx);
 /* Init function for filter installation */
 static ngx_int_t ngx_http_no_newlines_filter_init (ngx_conf_t *cf);
+
+/* Worker functions */
+static void ngx_http_no_newlines_strip_buffer (ngx_buf_t *buffer,
+                                               ngx_http_no_newlines_ctx_t *ctx);
+static void ngx_http_no_newlines_handle_tags (u_char *reader);
 
 
 /* Module directives */
 static ngx_command_t  ngx_http_no_newlines_commands[] = {
     { ngx_string ("no_newlines"),
       NGX_HTTP_LOC_CONF | NGX_CONF_NOARGS,
-      ngx_http_no_newlines,
+      NULL,
       0,
       0,
       NULL },
@@ -90,18 +97,20 @@ static ngx_http_output_body_filter_pt    ngx_http_next_body_filter;
 
 
 /* Function definitions start here */
+/*
 static char *ngx_http_no_newlines (ngx_conf_t *cf,
                                    ngx_command_t *cmd,
                                    void *conf)
 {
     ngx_http_core_loc_conf_t  *clcf;
-    /* Mine is a filter module, do I really need a handler? -VM */
+    // Mine is a filter module, do I really need a handler? -VM
     clcf = ngx_http_conf_get_module_loc_conf (cf, ngx_http_core_module);
     clcf->handler = ngx_http_no_newlines_handler;
 
     return NGX_CONF_OK;
 }
-
+*/
+ /*
 static ngx_int_t ngx_http_no_newlines_handler (ngx_http_request_t *r)
 {
     //ngx_buf_t    *b; //Not using this right now -VM
@@ -109,12 +118,26 @@ static ngx_int_t ngx_http_no_newlines_handler (ngx_http_request_t *r)
 
     return ngx_http_output_filter(r, &out);
 }
+*/
+
+
+static ngx_int_t ngx_http_no_newlines_filter_init (ngx_conf_t *cf)
+{
+    ngx_http_next_header_filter = ngx_http_top_header_filter;
+    ngx_http_top_header_filter = ngx_http_no_newlines_header_filter;
+
+    ngx_http_next_body_filter = ngx_http_top_body_filter;
+    ngx_http_top_body_filter = ngx_http_no_newlines_body_filter;
+
+    return NGX_OK;
+}
+
 
 static ngx_int_t ngx_http_no_newlines_header_filter (ngx_http_request_t *r)
 {
     ngx_http_no_newlines_ctx_t   *ctx;
 
-    /* step 1: decide whether to operate */
+    // step 1: decide whether to operate
     if ((r->headers_out.status != NGX_HTTP_OK &&
          r->headers_out.status != NGX_HTTP_FORBIDDEN &&
          r->headers_out.status != NGX_HTTP_NOT_FOUND) ||
@@ -122,12 +145,12 @@ static ngx_int_t ngx_http_no_newlines_header_filter (ngx_http_request_t *r)
         r->headers_out.content_type.len == 0 ||
         (r->headers_out.content_encoding &&
          r->headers_out.content_encoding->value.len)) {
-        /* No need to filter */
+        // No need to filter
         return ngx_http_next_header_filter(r);
     }
 
     if (ngx_strncasecmp(r->headers_out.content_type.data, (u_char *)"text/html", sizeof("text/html" - 1)) != 0) {
-        /* No need to filter */
+        // No need to filter
         return ngx_http_next_header_filter(r);
     }
 
@@ -160,6 +183,9 @@ static ngx_int_t ngx_http_no_newlines_body_filter (ngx_http_request_t *r,
         return ngx_http_next_body_filter(r, in);
     }
 
+    /* Set initial state to text */
+    ctx->state = state_text;
+
     /* For each buffer in the chain link, remove all the newlines */
     for (chain_link = in; chain_link; chain_link = chain_link->next) {
         ngx_http_no_newlines_strip_buffer (chain_link->buf, ctx);
@@ -182,9 +208,18 @@ static void ngx_http_no_newlines_strip_buffer (ngx_buf_t *buffer,
             case '\r':
             case '\n':
                 continue;
+            case '<':
+                ngx_http_no_newlines_handle_tags (reader);
+                break;
             default:
                 break;
             }
+            break;
+
+        case state_tag_pre:
+            //ignore newlines while we are displaying pre-formatted text
+            ngx_http_no_newlines_ignore_preformatted_text (reader);
+            ctx->state = state_text;
             break;
 
         case state_abort:
@@ -197,13 +232,38 @@ static void ngx_http_no_newlines_strip_buffer (ngx_buf_t *buffer,
     buffer->last = writer;
 }
 
-static ngx_int_t ngx_http_no_newlines_filter_init (ngx_conf_t *cf)
+static void ngx_http_no_newlines_handle_tags (u_char *reader)
 {
-    ngx_http_next_header_filter = ngx_http_top_header_filter;
-    ngx_http_top_header_filter = ngx_http_no_newlines_header_filter;
+    if (is_tag_pre (reader) == 1) {
+        ctx->state = state_tag_pre;
+    }
+}
 
-    ngx_http_next_body_filter = ngx_http_top_body_filter;
-    ngx_http_top_body_filter = ngx_http_no_newlines_body_filter;
+static ngx_int_t is_tag_pre (u_char *reader)
+{
+    u_char tagstring[4] = {0};
 
-    return NGX_OK;
+    reader++;
+    ngx_strlow (tagstring, reader, 3);
+
+    if (ngx_strncmp (tagstring, "pre", 3) == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+static void ngx_http_no_newlines_ignore_preformatted_text (u_char *reader)
+{
+    ngx_int_t task_complete = 0;
+
+    do {
+        while (*reader != '<')
+            reader++;
+        if (*(++reader) == '/') {
+            if (is_tag_pre (reader) == 1) {
+                task_complete = 1;
+            }
+        }
+    } while (task_complete == 0);
 }
